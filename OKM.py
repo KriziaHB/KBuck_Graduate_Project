4239b548229e0023d421e3f7b48834a9733b3f36
KriziaHB <kbuck1@cub.uca.edu>
2017-06-03 12:04:00 -0500

    Split Online K-Means methods into another file called OKM (Online K-Means) for simplicity and to separate modules

---
+++ OKM.py
@@ -0,0 +1,57 @@
+# Student: Krizia Houston Buck
+# Faculty: Dr. Emre Celebi
+# University of Central Arkansas
+# Summer 2017
+# Graduate Project
+# OKM.py (Online K-Means file)
+
+# PIL for manipulating images
+import PIL
+from PIL import Image
+
+# Online K-Means #
+def kmeans(k, pix, initC, pC, LR):
+    print("in kmeans")
+    end = 0
+
+    # iterate through pixels to form clusters
+    while (end < 5):
+        print("while")
+        end += 1
+
+    # return to main
+    out = 0
+    return(out)
+# end of kmeans #
+
+
+
+# Check Termination Criteria to see if met #
+def term():
+    print("in term")
+# end of term #
+
+
+
+# Randomizer for Initialization and Presentation Style #
+def rando():
+    print("in rando")
+# end of rando #
+
+
+
+# K Nearest Neighbor - find nearest centroid to current pixel #
+def knn():
+    print("in knn")
+# end of knn #
+
+
+
+# Update the Nearest Center (centroid) #
+def upC():
+    print("in upC")
+# end of upC #
+
+
+
+# #
