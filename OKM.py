c7bde32bfdc2f77426e6e963fa4e3fe80aeafed2
KriziaHB <kbuck1@cub.uca.edu>
2017-06-10 20:45:25 -0500

    Most Common Color Initialization and Random Initialization complete for k centroids

--- OKM.py
+++ OKM.py
@@ -9,30 +9,34 @@
 import PIL
 from PIL import Image
 from collections import Counter # ** currently not in use
+from random import randint
 
 
 
 # Online K-Means, call functions from here #
 def kmeans(k, im, pix, initC, psC, LR):
-    print("in kmeans")
+    print("--in kmeans")
     end = 0
 
+    width = int(im.size[0])
+    length = int(im.size[1])
 
-##****** COME BACK
+
+##****** COME BACK to Linear
     # Linear Initialiation
     if (initC == 1):
         print("Linear Init")
-    # Random Initialization
+    #! Random Initialization
     elif (initC == 2):
         print("Random Init")
-        rando(k, im, pix)
-    # Most Common Colors Initialization
+        centroids = randoI(k)
+    #! Most Common Colors Initialization
     elif (initC == 3):
         print("Most Common Color Init")
-        centroids = colorCount(k, im, pix)
-# ** CALL COUNTCOLOR FOR TESTING
+        centroids = colorCount(k, width, length, pix)
 
 
+    print("* Original Centroids *")
     print(centroids)
 
 
@@ -43,6 +47,7 @@ def kmeans(k, im, pix, initC, psC, LR):
     while (end < 2):
         print("while")
         end += 1
+## **
 
     # return to main
     # out = should be equal to whatever results from completed iterations, for now testing with out from colorCount
@@ -53,70 +58,96 @@ def kmeans(k, im, pix, initC, psC, LR):
 
 # Check Termination Criteria to see if met #
 def term():
-    print("in term")
+    print("--in term")
 # end of term #
 
 
 
-# Randomizer for Initialization and Presentation Style #
-def rando(k, im, pix):
-    print("in rando")
-# end of rando #
+# Randomizer for Initialization #
+def randoI(k):
+    print("--in rando")
+
+    count = 0
+    centroids = []
+
+    # go through until all centroid spots are filled with a unique color
+    while (count < k):
+
+        r1 = randint(0, 255)
+        r2 = randint(0, 255)
+        r3 = randint(0, 255)
+        r = (r1, r2, r3)
+        # print(r)
+
+        # if current random color is not a centroid, add it, or else repeat until unique
+        if r in centroids:
+            count = count
+        else:
+            centroids.append(r)
+            count += 1
+
+    return(centroids)
+# end of randoI #
+
+
+
+# Randomizer for Presentation Style #
+def randoP(k, width, length, pix):
+    print("--in rando")
+
+
+    #return(random)
+# end of randoP #
 
 
 
 # K Nearest Neighbor - find nearest centroid to current pixel #
 def knn():
-    print("in knn")
+    print("--in knn")
 # end of knn #
 
 
 
 # Update the Nearest Center (centroid) #
 def upC():
-    print("in upC")
+    print("--in upC")
 # end of upC #
 
 
 
 # Find Greatest Occurring Colors # ***** using for testing ******
-def colorCount(k, im, pix):
-    print("in colorCount - k = " + str(k))
+def colorCount(k, width, length, pix):
+    print("--in colorCount - k = " + str(k))
 
-
-    # print(Counter(r).most_common(k))
-  #  rpix = r.getpixel((0,5))
-
-    # HOW TO CHANGE PIXELS
-    # rint = 200
-    # test = im.load()
-    # for x in range(0,750):
-    #     for y in range(0,1334):
-    #         test[x,y] = (rint, rint, rint)
-    # im.show()
 
     # List for all pixels for counting
     pixList = []
 
-    for x in range(0,750):
-        for y in range(0,1334):
+    for x in range(0,width):
+        for y in range(0,length):
             pixList.append(pix[x,y])
-    print(pixList)
-    print(len(pixList))
+  #  print(pixList)
     cen = Counter(pixList).most_common(k)
     print(cen)
 
+    # After count is complete, only keep the pixel data (not the number of times it appears)
     centroids = []
     for element in cen:
         centroids.append(element[0])
-    print(centroids)
-    print(type(centroids)) #list
-    print(type(centroids[2])) #tuple
-    c = centroids[2]
-    print(c[0])
-    print(c[1])
-    print(c[2])
-    print(type(c[2])) #int
+    # centroids is a list of tuples containing ints
+    # replace individual centroids by assigning a new tuple to that centroid index
+
+
+ #   print(type(centroids)) #list
+ #   print(type(centroids[2])) #tuple
+ #   c = centroids[0]
+ #   print(type(c[2])) #int
+
+    # c = centroids[0]
+    # print(c)
+    # d = (12, 12, 12)
+    # centroids[0] = d
+    # print(centroids)
 
 
     return(centroids)
